5315bbe2cfddf1774f8f6ae024f781fc4732366d
Harrison Pearl <hpearl@Harrisons-MacBook-Pro.local>
2022-11-07 10:07:06 -0700

    pushed roll back to prev if needed

--- SfweDisplay/src/active_button.js
+++ SfweDisplay/src/active_button.js
@@ -7,20 +7,25 @@ home.addEventListener("click", function (){
     // tmp.className = 'Home_Popup'
     // tmp.id = 'modal'
     tmp = $('#pop_up_div').load('test.html')
-});
+    console.log(this.children[0].classList.toggle('closed'))
+},false);
 
 news = document.getElementById(2)
 news.addEventListener("click", function (){
     console.log("news is clicked")
-});
+    console.log(this.children[0].classList.toggle('closed'))
+},false);
 
 contact = document.getElementById(3)
 contact.addEventListener("click", function (){
     console.log("contact is clicked")
-});
+    console.log(this.children[0].classList.toggle('closed'))
+},false);
 
 about = document.getElementById(4)
 about.addEventListener("click", function (){
 
     console.log("about is clicked")
-});
+    // this.parentNode.parentNode.classList.toggle('closed')
+    console.log(this.children[1].classList.toggle('closed')) // toggles closed
+}, false);
