cc88ce18bed83db87e6af1074222e05efffe45c6
Javier <70613839+cjavierr@users.noreply.github.com>
2022-11-10 14:47:33 -0700

    Adding a x button to the modal to exit out

--- SfweDisplay/src/modal.js
+++ SfweDisplay/src/modal.js
@@ -2,10 +2,18 @@ modal = document.getElementById("modal")
 
 modalBtn = document.getElementById("mBtn")
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
 modalBtn.onclick = function(){
     modal.style.display = "block";
 }
 
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
 window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
