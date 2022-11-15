857c6e8f12c26d612a1be6f708183aae288ba34b
Javier <70613839+cjavierr@users.noreply.github.com>
2022-11-15 12:57:00 -0700

    Added qr images, image folder, and another modal under contanct us button

--- SfweDisplay/src/modal.js
+++ SfweDisplay/src/modal.js
@@ -1,21 +1,48 @@
-modal = document.getElementById("modal")
+qrmodal = document.getElementById("qrmodal")
 
-modalBtn = document.getElementById("mBtn")
+advBtn = document.getElementById("advBtn")
+advText =document.getElementById("advText")
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal for qr
+qrspan = document.getElementsByClassName("close")[0];
 
-modalBtn.onclick = function(){
-    modal.style.display = "block";
+advBtn.onclick = function(){
+  qrmodal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+qrspan.onclick = function() {
+    qrmodal.style.display = "none";
   }
   
 window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+    if (event.target == qrmodal) {
+      qrmodal.style.display = "none";
+    }
+}
+
+discmodal = document.getElementById("discmodal")
+
+discBtn = document.getElementById("discBtn")
+discText = document.getElementById("discText")
+
+// Get the <span> element that closes the modal for qr
+discspan = document.getElementsByClassName("close")[1];
+
+discBtn.onclick = function(){
+  discmodal.style.display = "block";
+  discText.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+discspan.onclick = function() {
+    discmodal.style.display = "none";
+    discText.style.display = "none";
+  }
+  
+window.onclick = function(event) {
+    if (event.target == discmodal) {
+      discmodal.style.display = "none";
+      discText.style.display = "none";
     }
 }
