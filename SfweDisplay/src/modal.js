1e6e9257b65307c3ab1f120e0aaefbbe4b7513a2
Javi <70613839+cjavierr@users.noreply.github.com>
2022-11-16 10:44:19 -0700

    Added slide show thingy and adjusted modals

--- SfweDisplay/src/modal.js
+++ SfweDisplay/src/modal.js
@@ -17,12 +17,12 @@ qrspan.onclick = function() {
     advText.style.display = "none";
   }
   
-window.onclick = function(event) {
-    if (event.target == qrmodal) {
-      qrmodal.style.display = "none";
-      advText.style.display = "none";
-    }
-}
+// window.onclick = function(event) {
+//     if (event.target == qrmodal) {
+//       qrmodal.style.display = "none";
+//       advText.style.display = "none";
+//     }
+// }
 
 discmodal = document.getElementById("discmodal")
 
@@ -48,4 +48,38 @@ window.onclick = function(event) {
       discmodal.style.display = "none";
       discText.style.display = "none";
     }
+
+    if (event.target == qrmodal) {
+      qrmodal.style.display = "none";
+      advText.style.display = "none";
+    }
 }
+
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// forward/Back controls
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
+
+//  images controls
+function currentSlide(n) {
+  SlideShow(slidePosition = n);
+}
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("Containers");
+  var circles = document.getElementsByClassName("dots");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < circles.length; i++) {
+      circles[i].className = circles[i].className.replace(" enable", "");
+  }
+  slides[slidePosition-1].style.display = "block";
+  circles[slidePosition-1].className += " enable";
+} 
