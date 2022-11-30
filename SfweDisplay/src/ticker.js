0c837ef7d4d4fa46a5abaf58ebe2523139f2cc1d
Harrison Pearl <hpearl@dhcp-10-142-240-99.uawifi.arizona.edu>
2022-11-30 14:37:47 -0700

    working ticker

--- SfweDisplay/src/ticker.js
+++ SfweDisplay/src/ticker.js
@@ -1,27 +1,30 @@
 const fs = require('fs')
 var txt = document.getElementById('tick')
-
-  
+let tarr;
+let numPrompts;
+let pindex = 0
 fs.readFile('src/ticker_text/ticker.txt', (err, data) => {
     if (err) throw err;
-  
     console.log(data.toString());
     let file_content = data.toString()
-    txt.innerText = file_content;
+    tarr = data.toString().split(/\r?\n/).filter(element => element);
+    txt.innerText = '    🐻⬇️    ' + tarr[0] + '    🐻⬇️    '
+    numPrompts = tarr.length;
+
 })
-
-
-
-var width = $('.ticker-text').width(),
+$(document).ready(function(e){
     containerwidth = $('.ticker-container').width(),
     left = containerwidth;
-$(document).ready(function(e){
-	function tick() {
-        if(--left < -width){
+    sep = '    🐻⬇️    '
+    function tick() {
+        width = $('.ticker-text h3').width()
+        if(--left <= -width){
             left = containerwidth;
+            pindex = (pindex + 1) % (numPrompts)
+            txt.innerText = sep + tarr[pindex] + sep
         }
-        $(".ticker-text").css("margin-left", left + "px");
-        setTimeout(tick, 8);
-      }
-      tick();
+        $(".ticker-text h3").css("margin-left", left + "px");
+        setTimeout(tick, 4);
+    }
+    tick();
 });
