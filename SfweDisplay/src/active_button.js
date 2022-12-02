d3f1bf879b740b426ed7c79e1992b04bbc15818d
Javier <70613839+cjavierr@users.noreply.github.com>
2022-12-02 12:07:20 -0700

    pushing

--- SfweDisplay/src/active_button.js
+++ SfweDisplay/src/active_button.js
@@ -1,29 +1,11 @@
 window.$ = window.jQuery = require('jquery');
 
-var home = document.getElementById(1)
+
 var news = document.getElementById(2)
 var contact = document.getElementById(3)
 var about = document.getElementById(4)
 
-home.addEventListener("click", function (){
-    // console.log("home is clicked");
-    // tmp = document.body.appendChild(document.createElement('div'))
-    // tmp.className = 'Home_Popup'
-    // tmp.id = 'modal'
-    // $('#result').append($('<div>').load('ajax/test.html'));
-    var tmp = $('#pop_up_div').load('test.html');
-    this.children[1].classList.toggle('closed');
-    if(!news.children[1].classList.contains('closed')){
-        news.children[1].classList.toggle('closed');
-    }
-    if(!about.children[1].classList.contains('closed')){
-        about.children[1].classList.toggle('closed');
-    }
-    if(!contact.children[1].classList.contains('closed')){
-        contact.children[1].classList.toggle('closed');
-    }
 
-}, false);
 
 
 news.addEventListener("click", function (){
