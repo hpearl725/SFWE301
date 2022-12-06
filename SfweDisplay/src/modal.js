61748c5ff926b4731f91578b94fe6739e5392525
Javier <70613839+cjavierr@users.noreply.github.com>
2022-12-05 18:29:39 -0700

    Adding minor pdf and undergrad pdf and commenting modal.js

--- SfweDisplay/src/modal.js
+++ SfweDisplay/src/modal.js
@@ -1,5 +1,10 @@
-advmodal = document.getElementById("advmodal")
+/*
+JS logic to make the individual modals appear and disappear
+, achieved by changing display style of the modals 
+and their contents between block and none 
+*/
 
+advmodal = document.getElementById("advmodal")
 advBtn = document.getElementById("advBtn")
 advText =document.getElementById("advText")
 
@@ -11,7 +16,8 @@ advspan.onclick = function() {
   advmodal.style.display = "none";
   advText.style.display = "none";
 }  
-
+// upon clicking the relevant button display the modal, closing it
+//  if any portion of the screen not within the content is clicked
 advBtn.onclick = function(){
   advmodal.style.display = "block";
   advText.style.display = "block";
@@ -26,13 +32,14 @@ advBtn.onclick = function(){
 
 
 discmodal = document.getElementById("discmodal")
-
 discBtn = document.getElementById("discBtn")
 discText = document.getElementById("discText")
 
 // Get the <span> element that closes the modal for qr
 discspan = document.getElementById("discclose");
 
+// upon clicking the relevant button display the modal, closing it
+//  if any portion of the screen not within the content is clicked
 discBtn.onclick = function(){
   discmodal.style.display = "block";
   discText.style.display = "block";
@@ -53,14 +60,13 @@ discspan.onclick = function() {
   }
   
 
-
 quizmodal = document.getElementById("quizmodal")
-
 quizBtn = document.getElementById("quizBtn")
-
 // Get the <span> element that closes the modal for qr
 quizspan = document.getElementById("quizclose");
 
+// upon clicking the relevant button display the modal, closing it
+//  if any portion of the screen not within the content is clicked
 quizBtn.onclick = function(){
   quizmodal.style.display = "block";
   //quizText.style.display = "block";
@@ -78,40 +84,57 @@ quizspan.onclick = function() {
     //quizText.style.display = "none";
 }
 
+
 ungradmodal = document.getElementById("ungradmodal")
-
 ungradBtn = document.getElementById("ungradBtn")
-
 // Get the <span> element that closes the modal for qr
 ungradspan = document.getElementById("ungradclose");
 
+// upon clicking the relevant button display the modal, closing it
+//  if any portion of the screen not within the content is clicked
 ungradBtn.onclick = function(){
   ungradmodal.style.display = "block";
   window.onclick = function(event) {
     if (event.target == ungradmodal) {
       ungradmodal.style.display = "none";
-      //ungradText.style.display = "none";
     }
 }
-  //ungradText.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 ungradspan.onclick = function() {
     ungradmodal.style.display = "none";
-    //ungradText.style.display = "none";
-    
-  
-
 }
 
+minormodal = document.getElementById("minormodal")
+minorBtn = document.getElementById("minorBtn")
+// Get the <span> element that closes the modal for qr
+minorspan = document.getElementById("minorclose");
+
+// upon clicking the relevant button display the modal, closing it
+//  if any portion of the screen not within the content is clicked
+minorBtn.onclick = function(){
+  minormodal.style.display = "block";
+  window.onclick = function(event) {
+    if (event.target == minormodal) {
+      minormodal.style.display = "none";
+    }
+}
+}
+
+// When the user clicks on <span> (x), close the modal
+minorspan.onclick = function() {
+    minormodal.style.display = "none";
+}
+
+
 whtmodal = document.getElementById("whtmodal")
-
 whtBtn = document.getElementById("whtBtn")
-
 // Get the <span> element that closes the modal for qr
 whtspan = document.getElementById("whtclose");
 
+// upon clicking the relevant button display the modal, closing it
+//  if any portion of the screen not within the content is clicked
 whtBtn.onclick = function(){
   whtmodal.style.display = "block";
   window.onclick = function(event) {
@@ -124,9 +147,10 @@ whtBtn.onclick = function(){
 // When the user clicks on <span> (x), close the modal
 whtspan.onclick = function() {
     whtmodal.style.display = "none";
-    
 }
 
+// logic for the slideshow, advnacing a slide position variable
+// upon clicking the relevant buttons
 var slidePosition = 1;
 SlideShow(slidePosition);
 
